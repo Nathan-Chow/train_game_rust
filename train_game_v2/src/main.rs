e590ca8fd19574a85735602daf0b2e3b92d3f27e
Nathan Chow <nathanmchow@gmail.com>
2023-05-15 23:26:51 +1000

    Basic working post request

--- train_game_v2/src/main.rs
+++ train_game_v2/src/main.rs
@@ -3,19 +3,40 @@ pub mod calcs;
 
 use crate::multithread::solve;
 
-use actix_web::{get, web::ServiceConfig};
+use actix_web::{post, web::{ServiceConfig, self}, HttpResponse};
 use shuttle_actix_web::ShuttleActixWeb;
+use serde::{Deserialize, Serialize};
 
-#[get("/")]
-async fn hello_world() -> &'static str {
-    "Hello World!"
+#[derive(Deserialize)]
+struct TrainPayload {
+    numbers: String
+}
+
+#[derive(Serialize)]
+struct ResponseBody {
+    all_solutions: Vec<String>,
+    num_solutions: i32,
+}
+
+#[post("/train_game")]
+async fn train_game(payload: web::Json<TrainPayload>) -> HttpResponse {
+    let number = payload.numbers.clone();
+
+    let all_solutions = solve(number);
+    let num_solutions = all_solutions.len() as i32;
+    let response_body = ResponseBody {
+        all_solutions,
+        num_solutions
+    };
+
+    HttpResponse::Ok().json(response_body)
 }
 
 #[shuttle_runtime::main]
 async fn actix_web(
 ) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(hello_world);
+        cfg.service(train_game);
     };
 
     Ok(config.into())
